81390a6c6cc86b2828006aebecdfacdb32fee210
Andrew Perotti <Perotti28@gmail.com>
2025-10-01 09:09:30 -0700

    feat: remove content from previous project
    This results is a bare bones template, which I should save as a repo unto itself so I don't have to do this delete step in the future.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,12 @@
 import { useState } from "react";
 import "../styles/App.css";
-import General from "./General.jsx";
-import Education from "./Education.jsx";
-import Professional from "./Professional.jsx";
+
 
 function App() {
-  const [person, setPerson] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    location: "",
-    title: "",
-    description: "",
-    skills: [""],
-    education: [
-      {
-        institutionName: "",
-        courseTitle: "",
-        startDate: "2000-01",
-        endDate: "",
-      },
-    ],
-    professional: [
-      {
-        companyName: "",
-        positionHeld: "",
-        startDate: "2000-01",
-        endDate: "",
-        responsibilities: [""],
-      },
-    ],
-  });
+ 
 
   return (
-    <div className="App">
-      <h1 id="title">CV Builder</h1>
-      <General person={person} setPerson={setPerson} />
-      <Education person={person} setPerson={setPerson} />
-      <Professional person={person} setPerson={setPerson} />
-    </div>
+
   );
 }
 
