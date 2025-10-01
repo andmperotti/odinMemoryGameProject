a36b33e8b0a7f343e4abd49d4f41d70edb5c203c
Andrew Perotti <Perotti28@gmail.com>
2025-10-01 11:18:18 -0700

    feat: add fetch call to espn api
    This api will provide us with abbreviations and logos for all 30 major league baseball teams.
    In addition I've added a variable that holds the specific array element of that api return object which has the team data.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import "../styles/App.css";
 import {shuffle} from '../assets/functions'
 
+let mlbData = await fetch(
+  "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams"
+).then((result) => result.json());
+let mlbTeams = mlbData.sports[0].leagues[0].teams;
 
 function App() {
  
